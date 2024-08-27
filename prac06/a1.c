994e6f5cae2e7f752b587742e2df763d4691e3be
Rodo <rodonguyendd@gmail.com>
2024-08-27 18:26:41 +1000

    prac06 a2

--- prac06/a1.c
+++ prac06/a1.c
@@ -1,29 +1,31 @@
 #include <pthread.h>
 #include <stdio.h>
-#include <string.h>    /* String handling */
-
+#include <string.h> /* String handling */
 
 void *addMillion(void *arg);
-int globalVar =0;
+int globalVar = 0;
 
-int main(int argc, char *argv[]) {
-	pthread_t thread1, thread2;
+int main(int argc, char *argv[])
+{
+  pthread_t thread1, thread2;
   // pthread_t thread3;
-    pthread_create(&thread1, NULL, addMillion, NULL);
-    pthread_create(&thread2, NULL, addMillion, NULL);
-    // pthread_create(&thread3, NULL, addMillion, NULL);
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);   
-    // pthread_join(thread3, NULL);   
-    printf("\n\n2 Threads: globalVar = %d\n", globalVar);
-    return 0;
+  pthread_create(&thread1, NULL, addMillion, NULL);
+  pthread_create(&thread2, NULL, addMillion, NULL);
+  // pthread_create(&thread3, NULL, addMillion, NULL);
+  pthread_join(thread1, NULL);
+  pthread_join(thread2, NULL);
+  // pthread_join(thread3, NULL);
+  printf("\n\n2 Threads: globalVar = %d\n", globalVar);
+  return 0;
 }
 
-void *addMillion(void *ptr) {
-	
-    for (int i = 0; i < 1000000; i++) {
-       globalVar++;
-    }
-    
-    return NULL;
+void *addMillion(void *ptr)
+{
+
+  for (int i = 0; i < 1000000; i++)
+  {
+    globalVar++;
+  }
+
+  return NULL;
 }
--- prac06/a2_work.c
+++ prac06/a2_work.c
@@ -1,9 +1,10 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <string.h> /* String handling */
 #include <sys/time.h>
 
 void *addMillion(void *arg);
-int globalVar = 0;
+int globalSum = 0;
 pthread_mutex_t lock;
 
 int main(int argc, char *argv[])
@@ -15,11 +16,14 @@ int main(int argc, char *argv[])
   gettimeofday(&start, NULL);
 
   pthread_t thread1, thread2;
+  pthread_t thread3;
   pthread_create(&thread1, NULL, addMillion, NULL);
   pthread_create(&thread2, NULL, addMillion, NULL);
+  pthread_create(&thread3, NULL, addMillion, NULL);
   pthread_join(thread1, NULL);
   pthread_join(thread2, NULL);
-  printf("Total for globalVar = %d\n", globalVar);
+  pthread_join(thread3, NULL);
+  printf("3 Threads: globalSum = %d\n", globalSum);
 
   gettimeofday(&end, NULL);
   double elapsedTime = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
@@ -30,20 +34,16 @@ int main(int argc, char *argv[])
 
 void *addMillion(void *ptr)
 {
-  if (pthread_mutex_lock(&lock))
-  {
-    perror("\nError with mutex lock\n");
-  }
-  
+  int localSum = 0;
+
   for (int i = 0; i < 1000000; i++)
   {
-    globalVar++;
+    localSum++;
   }
 
-  if (pthread_mutex_unlock(&lock))
-  {
-    perror("\nError unlocking mutex\n");
-  }
+  pthread_mutex_lock(&lock);
+  globalSum += localSum;
+  pthread_mutex_unlock(&lock);
   
   return NULL;
 }
