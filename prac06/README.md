f4d2fbae7e52c002c9f5e3e576948b2484644d8c
Rodo <rodonguyendd@gmail.com>
2024-08-29 19:48:14 +1000

    prac06 a3

--- prac06/README.md
+++ prac06/README.md
@@ -1,2 +1,17 @@
 - The manual/docs for C libraries are not often beginner-friendly. I'd recommend googling with "example" keyword, or using AI.
     ![Image](./a2.png)
+
+- `sem_post()` and `sem_wait()`:
+    sem_post:
+
+    Increments the value of a semaphore by 1.
+    If there are waiting threads (semaphores have a value of 0), it wakes up one of them, allowing it to proceed.
+    If there are no waiting threads, sem_post does nothing.
+    sem_wait:
+
+    Decrements the value of a semaphore by 1.
+    If the semaphore’s value is initially 0, the calling thread will block until another thread calls sem_post to increment the value and wake up the waiting thread.
+    If the semaphore’s value is initially positive, sem_wait will decrement it and allow the calling thread to proceed.
+    In summary, sem_post releases a semaphore, potentially waking up waiting threads, while sem_wait acquires a semaphore, potentially blocking if it’s already at 0.
+
+    Regarding the question about calling sem_post before sem_wait, the answer is that it will work as expected. sem_post simply increments the semaphore, and if there are waiting threads, it will wake them up. The order of calls does not matter, as long as the semaphore’s value is properly managed.
--- prac06/skeleton_buffer.c
+++ prac06/skeleton_buffer.c
@@ -10,92 +10,137 @@
 buffer_item buffer[BUFFER_SIZE];
 pthread_mutex_t mutex;
 sem_t empty; // semaphore to keep track of empty slots in the buffer
-sem_t full; // semaphore to keep track of full slots in the buffer
+sem_t full;  // semaphore to keep track of full slots in the buffer
 
 int insertPointer = 0, removePointer = 0;
 
-void *producer(void *param);
-void *consumer(void *param);
+void *producer(int thread_number);
+void *consumer(int thread_number);
 
-int insert_item(buffer_item item) {
-	//Acquire Empty Semaphore
-	sem_wait(&empty);
+int insert_item(buffer_item item)
+{
+  // Acquire Empty Semaphore
+  sem_wait(&empty); // add 1 to "empty"
 
-	//Acquire mutex lock to protect buffer
-	pthread_mutex_lock(&mutex);
-	buffer[insertPointer++] = item;
-	insertPointer = insertPointer % BUFFER_SIZE;
+  // Acquire mutex lock to protect buffer
+  pthread_mutex_lock(&mutex);
+  buffer[insertPointer++] = item;
+  insertPointer = insertPointer % BUFFER_SIZE;
 
-	//Release mutex lock and full semaphore
-	pthread_mutex_unlock(&mutex);
-	sem_post(&full);
+  // print
+	int val1 = 0, val2 = 0;
+	sem_getvalue(&full, &val1);
+	sem_getvalue(&empty, &val2);
+  printf("\tSemaphore full = %d, empty = %d\n", val1, val2);
+  printf("\tremovePointer = %d, insertPointer = %d\n", removePointer, insertPointer);
 
-	return 0;
+  // Release mutex lock and full semaphore
+  pthread_mutex_unlock(&mutex);
+  sem_post(&full);
+
+  return 0;
 }
 
-int remove_item(buffer_item *item) {
-	/* insert your code here for Task 1 (a) */
+int remove_item(buffer_item *item)
+{
+  /* Acquire Full Semaphore */
+  sem_wait(&full); // add 1 to "full"
 
-	/* Acquire Full Semaphore */
+  /* Acquire mutex lock to protect buffer */
+  pthread_mutex_lock(&mutex);
+  *item = buffer[removePointer];
+  buffer[removePointer] = -1;
+  removePointer = (removePointer + 1) % BUFFER_SIZE;
 
-	/* Acquire mutex lock to protect buffer */
+  // print
+  printf("\tremovePointer = %d, insertPointer = %d\n", removePointer, insertPointer);
 
-	/* Release mutex lock and empty semaphore */
+  /* Release mutex lock and empty semaphore */
+  pthread_mutex_unlock(&mutex);
+  sem_post(&empty); // minus 1 to "empty"
 
-	return 0;
+  return 0;
 }
 
-int main(int argc, char *argv[]) {
-	int sleepTime, producerThreads, consumerThreads;
-	int i, j;
+int main(int argc, char *argv[])
+{
+  if (argc != 4)
+  {
+    fprintf(stderr, "Useage: <sleep time> <producer threads> <consumer threads>\n");
+    return -1;
+  }
 
-	if (argc != 4) 	{
-		fprintf(stderr, "Useage: <sleep time> <producer threads> <consumer threads>\n");
-		return -1;
-	}
+  /* insert you code below for Task 1 (b) */
+  /* Get command line arguments argv[1], argv[2], argv[3]*/
+  int sleepTime = atoi(argv[1]);
+  int producerThreadNumber = atoi(argv[2]);
+  int consumerThreadNumber = atoi(argv[3]);
 
-	/* insert you code below for Task 1 (b) */
+  // Create arrays of thread ids
+  pthread_t producer_thread_ids[producerThreadNumber];
+  pthread_t consumer_thread_ids[consumerThreadNumber];
 
-	/* Get command line arguments argv[1], argv[2], argv[3]*/
+  /* Initialize the the locks */
+  pthread_mutex_init(&mutex, NULL);
+  sem_init(&empty, 0, BUFFER_SIZE);
+  sem_init(&full, 0, 0);
 
-	/* Initialize the the locks */
+  /* Create the producer and consumer threads */
+  for (int i = 0; i < producerThreadNumber; i++)
+  {
+    pthread_create(&producer_thread_ids[i], NULL, producer, i + 1);
+  }
 
-	/* Create the producer and consumer threads */
+  for (int j = 0; j < consumerThreadNumber; j++)
+  {
+    pthread_create(&consumer_thread_ids[j], NULL, consumer, j + 1);
+  }
 
-	/* Sleep for user specified time */
+  /* Sleep for user specified time */
+  sleep(sleepTime);
 
-	return 0;
+  return 0;
 }
 
-void *producer(void *param) {
-	buffer_item random;
-	int r;
+void *producer(int thread_number)
+{
+  buffer_item random;
+  int r;
 
-	while (TRUE) 	{
-		r = rand() % 5;
-		sleep(r);
-		random = rand();
+  while (TRUE)
+  {
+    r = rand() % 5;
+    sleep(r);
+    random = rand();
 
-		if (insert_item(random)) {
-			fprintf(stderr, "Error");
-		} else {
-			printf("Producer produced %d \n", random);
-		}
-	}
+    if (insert_item(random))
+    {
+      fprintf(stderr, "Error");
+    }
+    else
+    {
+      printf("Producer %d produced %d \n", thread_number, random);
+    }
+  }
 }
 
-void *consumer(void *param) {
-	buffer_item random;
-	int r;
+void *consumer(int thread_number)
+{
+  buffer_item random;
+  int r;
 
-	while (TRUE) 	{
-		r = rand() % 5;
-		sleep(r);
+  while (TRUE)
+  {
+    r = rand() % 5;
+    sleep(r);
 
-		if (remove_item(&random)) {
-			fprintf(stderr, "Error Consuming");
-		} else {
-			printf("Consumer consumed %d \n", random);
-		}
-	}
+    if (remove_item(&random))
+    {
+      fprintf(stderr, "Error Consuming");
+    }
+    else
+    {
+      printf("Consumer %d consumed %d \n", thread_number, random);
+    }
+  }
 }
